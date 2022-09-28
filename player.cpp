528c495b0cbc0df886ae226562fc3eb42e955c68
Gachiyabakune <fko2247097@stu.o-hara.ac.jp>
2022-09-28 11:12:38 +0900

    ジャンプ処理　当たり判定

--- player.cpp
+++ player.cpp
@@ -2,13 +2,21 @@
 #include "game.h"
 #include "player.h"
 
+namespace
+{
+	//キャラクターのサイズ
+	constexpr float kSizeX = 128.0f;
+	constexpr float kSizeY = 128.0f;
+
+	//ジャンプ力
+	constexpr float kJumpAcc = -16.0f;
+	//重力
+	constexpr float kGravity = 0.8f;
+}
 Player::Player()
 {
 	m_handle = -1;
 	m_fieldY = 0.0f;
-
-	m_isJumpUp = false;
-	m_isJumpDown = false;
 
 	m_isDead = false;
 }
@@ -32,31 +40,26 @@ void Player:: setup(float fieldY)
 
 void Player::update()
 {
-	//m_pos += m_vec;
+	if (m_isDead) return;
+	m_pos += m_vec;
+	//地面との当たり
+	bool isField = false;
+	if (m_pos.y > m_fieldY - m_graphSize.y)
+	{
+		m_pos.y = m_fieldY - m_graphSize.y;
+		isField = true;
+	}
 
 	// キー入力処理
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (padState & PAD_INPUT_1)
 	{
-		m_isJumpUp = true;
-	}
-	if (m_isJumpUp)
-	{
-		m_pos.y -= 4.0f;
-		if (m_pos.y <= 64.0f)
+		if (isField)
 		{
-			m_isJumpUp = false;
-			m_isJumpDown = true;
+			m_vec.y = kJumpAcc;	//ジャンプ開始
 		}
 	}
-	else if (m_isJumpDown)
-	{
-		m_pos.y += 4.0f;
-		if (m_pos.y >= m_fieldY - m_graphSize.y)
-		{
-			m_pos.y = m_fieldY - m_graphSize.y;
-		}
-	}
+	m_vec.y += kGravity;
 }
 
 void Player::draw()
